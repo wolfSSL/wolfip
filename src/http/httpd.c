358496288ce90f9670091df3e872b58f59aa8fbe
Daniele Lacamera <dan@danielinux.net>
2025-02-24 13:51:53 +0100

    Fixed copyright statements

--- src/http/httpd.c
+++ src/http/httpd.c
@@ -1,6 +1,22 @@
-/* HTTP 1.1 server
- * (c) Danielinux 2024 <root@danielinux.net>
- * This code is licensed under the GPLv3 license.
+/* httpd.c
+ *
+ * Copyright (C) 2024 wolfSSL Inc.
+ *
+ * This file is part of wolfIP TCP/IP stack.
+ *
+ * wolfIP is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * wolfIP is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1335, USA
  */
 #include "wolfip.h"
 #include "httpd.h"
