27cd129dff3c76f2cd1c74bc753f9ec7a2477aba
David Garske <david@wolfssl.com>
2026-05-11 09:39:14 -0700

    stm32h5: TZEN=1 NS app under wolfBoot

--- src/port/stm32/stm32_eth.h
+++ src/port/stm32/stm32_eth.h
@@ -16,5 +16,12 @@ int stm32_eth_init(struct wolfIP_ll_dev *ll, const uint8_t *mac);
 void stm32_eth_get_stats(uint32_t *polls, uint32_t *pkts);
 uint32_t stm32_eth_get_rx_des3(void);
 uint32_t stm32_eth_get_dmacsr(void);
+#ifdef DEBUG_H5_ETH
+uint32_t stm32_eth_get_tx_count(void);
+uint32_t stm32_eth_get_tx_des3(uint32_t i);
+uint32_t stm32_eth_get_mac_rx_frames(void);
+uint32_t stm32_eth_get_mac_tx_frames(void);
+uint32_t stm32_eth_get_mac_rx_errors(void);
+#endif
 
 #endif /* WOLFIP_STM32_ETH_H */
