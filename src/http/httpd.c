e6654c586e6f1f608d3361b8fb0ba4e0a020b10a
Daniele Lacamera <root@danielinux.net>
2026-07-01 16:51:38 +0200

    httpd: revert branch free-slot fix, superseded by master
    The free-slot check was addressed independently on master in f3c807f
    (track route slot occupancy by path). Restore src/http/httpd.c to the
    master version so this PR carries only documentation changes.

--- src/http/httpd.c
+++ src/http/httpd.c
@@ -70,15 +70,10 @@ static const char *http_status_text(int status_code) {
     }
 }
 
-static int http_url_slot_is_free(const struct http_url *url)
-{
-    return (url->handler == NULL) && (url->static_content == NULL);
-}
-
 int httpd_register_handler(struct httpd *httpd, const char *path, int (*handler)(struct httpd *httpd, struct http_client *hc, struct http_request *req)) {
-    int i = 0;
+    int i;
     for (i = 0; i < HTTPD_MAX_URLS; i++) {
-        if (http_url_slot_is_free(&httpd->urls[i])) {
+        if (httpd->urls[i].path[0] == '\0') {
             /* Copy path and guarantee null termination */
             strncpy(httpd->urls[i].path, path, HTTP_PATH_LEN);
             httpd->urls[i].path[HTTP_PATH_LEN-1] = '\0';
@@ -90,9 +85,9 @@ int httpd_register_handler(struct httpd *httpd, const char *path, int (*handler)
 }
 
 int httpd_register_static_page(struct httpd *httpd, const char *path, const char *content) {
-    int i = 0;
+    int i;
     for (i = 0; i < HTTPD_MAX_URLS; i++) {
-        if (http_url_slot_is_free(&httpd->urls[i])) {
+        if (httpd->urls[i].path[0] == '\0') {
             /* Copy path and guarantee null termination */
             strncpy(httpd->urls[i].path, path, HTTP_PATH_LEN);
             httpd->urls[i].path[HTTP_PATH_LEN-1] = '\0';
